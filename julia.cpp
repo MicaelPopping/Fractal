dd7909d6275ed1b32fffd44ae1fd289d4109e788
Micael Popping <mmpopping@inf.ufpel.edu.br>
2019-07-14 14:37:38 -0300

    Att julia

--- julia.cpp
+++ julia.cpp
@@ -17,28 +17,25 @@ Julia::Julia()
 
 void Julia::generate_image() {
 
-    if(fractal_model == "One") {
-		ofstream fout("One.ppm");
-		fout << "P3" << endl; // Magic Number extensão ppm
-		fout << image_width << " " << image_height << endl;
-		fout << "256" << endl; // Valor máximo de cor do pixel RGB
-		for(int y = 0; y < image_height; ++y) {
-			for(int x = 0; x < image_width; ++x) {
-				double cr = map_To_Real(x);
-				double ci = map_To_Imaginary(y);
-				double NumberR = -0.70176;
-				double NumberI = -0.3842;
-				int n = generate(cr, ci, NumberR, NumberI);
+	ofstream fout("julia.ppm");
 
-				int r = (n  % color_r); // CORES
-				int g = (n * 3 % color_g);
-				int b = (n % color_b);
+	fout << "P3" << endl; // Magic Number extensão ppm
+	fout << image_width << " " << image_height << endl;
+	fout << "256" << endl; // Valor máximo de cor do pixel RGB
 
-				fout << r << " " << g << " " << b << " ";
-			}
-				fout << endl;
+	for(int y = 0; y < image_height; ++y) {
+		for(int x = 0; x < image_width; ++x) {
+
+			int n = generate(x, y, -0.70176, -0.3842);
+
+			int r = (n  % color_r); // CORES
+			int g = (n * 3 % color_g);
+			int b = (n % color_b);
+            
+			fout << r << " " << g << " " << b << " ";
 		}
-				fout.close();
-				cout << "Terminado! " << fractal_model << " gerado com sucesso!" << endl;
+			fout << endl;
 	}
+			fout.close();
+			cout << "Terminado! " << fractal_model << " gerado com sucesso!" << endl;
 }
